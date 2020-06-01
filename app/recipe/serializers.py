0f847fa906d62fd72abe276bbc8c3fd03be79e58
mattbee <matt.bee@madetech.com>
2020-06-01 12:12:55 +0100

    Add recipe image feature

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -46,3 +46,11 @@ class RecipeDetailSerializer(RecipeSerializer):
     """Serialize a recipe detail"""
     ingredients = IngredientSerializer(many=True, read_only=True)
     tags = TagSerializer(many=True, read_only=True)
+
+
+class RecipeImageSerializer(serializers.ModelSerializer):
+    """Serialize a recipe image"""
+    class Meta:
+        model = Recipe
+        fields = ('id', 'image')
+        read_only_fields = ('id',)
